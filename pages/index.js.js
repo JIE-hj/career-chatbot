bcbabf1cd6a23127bec5ae7596dc5659ec48ca26
JIE-hj <jihj9616@gmail.com>
2025-05-31 00:00:01 +0900

    Delete pages directory

--- pages/index.js.js
+++
@@ -1,9 +0,0 @@
-// pages/index.js
-export default function Home() {
-  return (
-    <div style={{ padding: '50px', fontFamily: 'sans-serif' }}>
-      <h1>진로 추천 GPT 챗봇</h1>
-      <p>챗봇이 정상적으로 작동하고 있어요!</p>
-    </div>
-  );
-}
